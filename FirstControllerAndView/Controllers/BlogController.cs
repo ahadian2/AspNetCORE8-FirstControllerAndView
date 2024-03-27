14fcda234ef4dc70fae5b97451a6d98988a61aaf
Mohammadreza <ahadian2@gmail.com>
2024-03-28 00:22:52 +0330

    Add project files.

---
+++ FirstControllerAndView/Controllers/BlogController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FirstControllerAndView.Controllers
+{
+    public class BlogController : Controller
+    {
+        public IActionResult Post() { 
+        
+        return View("Category");
+        }
+
+        public IActionResult Category()
+        {
+            return View("Post");
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
